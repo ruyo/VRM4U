c9032c22bbd8cdab0cce70308d64bb0678c0110a
ruyo <ruyoruyo@gmail.com>
2023-11-25 22:50:40 +0900

    fix: vrm10判定がずれることがあるのを修正

--- Source/VRM4ULoader/Private/VrmJson.cpp
+++ Source/VRM4ULoader/Private/VrmJson.cpp
@@ -41,13 +41,31 @@ void VrmJsonTest(const uint8_t* pData, size_t size) {
 
 bool VRMIsVRM10(const uint8_t* pData, size_t size) {
 
+	if (size < 4 || pData == nullptr) {
+		return false;
+	}
+
 	int c = 0;
 	size_t c_start = 0;
-	for (; c_start < size; ++c_start) {
-		if (pData[c_start] == '{') {
-			break;
+	{
+		std::vector<char> str = { 'J','S','O','N' };
+
+		for (; c_start < size - str.size(); ++c_start) {
+			bool bFound = false;
+
+			for (int i = 0; i < 4; ++i) {
+				if (str[i] != pData[c_start + i]) {
+					continue;
+				}
+				bFound = true;
+			}
+			if (bFound) {
+				c_start += 4;
+				break;
+			}
 		}
 	}
+
 	size_t c_end = c_start;
 	for (; c_end < size; ++c_end) {
 		if (pData[c_end] == '{') {
@@ -76,34 +94,4 @@ bool VRMIsVRM10(const uint8_t* pData, size_t size) {
 		}
 	}
 	return false;
-
-
-
-
-
-	/*
-	std::string s = j["extensions"]["VRM"]["exporterVersion"];
-	auto s2 = j["extensions"]["VRM"]["meta"]["title"].get <std::string>();
-	auto s3 = UTF8_TO_TCHAR(s2.c_str());
-
-  auto v1 = json["hoge"]["fuga"][1];
-  auto v2 = json["/hoge/fuga/1"_json_pointer];
-  auto key = nlohmann::json::json_pointer("/hoge/fuga/1");
-  auto v3 = json[key];
-
-	// add a number that is stored as double (note the implicit conversion of j to an object)
-	j["pi"] = 3.141;
-
-	// add a Boolean that is stored as bool
-	j["happy"] = true;
-
-	// add a string that is stored as std::string
-	j["name"] = "Niels";
-
-	// add another null object by passing nullptr
-	j["nothing"] = nullptr;
-
-	// add an object inside the object
-	j["answer"]["everything"] = 42;
-	*/
 }
