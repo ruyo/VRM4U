6afeacdc6717fbdfeb451a41a728b2a6fae0eb40
ruyo <ruyoruyo@gmail.com>
2024-03-20 22:52:33 +0900

    4.20のエラー対処

--- Source/ReleaseScript/version.ps1
+++ Source/ReleaseScript/version.ps1
@@ -14,7 +14,7 @@ if ($a.EngineAssociation -eq '4.23' -or $a.EngineAssociation -eq '4.22' -or $a.E
     $b | ConvertTo-Json > ../../../VRM4U/VRM4U.uplugin
 }
 
-if ($a.EngineAssociation -eq '4.27' -or $a.EngineAssociation -eq '4.26' -or $a.EngineAssociation -eq '4.25' -or $a.EngineAssociation -eq '4.24' -or $a.EngineAssociation -eq '4.23' -or $a.EngineAssociation -eq '4.22' -or $a.EngineAssociation -eq '4.21')
+if ($a.EngineAssociation -eq '4.27' -or $a.EngineAssociation -eq '4.26' -or $a.EngineAssociation -eq '4.25' -or $a.EngineAssociation -eq '4.24' -or $a.EngineAssociation -eq '4.23' -or $a.EngineAssociation -eq '4.22' -or $a.EngineAssociation -eq '4.21' -or $a.EngineAssociation -eq '4.20')
 {
 
     $b = Get-Content ../../../VRM4U/VRM4U.uplugin -Encoding UTF8 | ConvertFrom-Json
@@ -27,7 +27,7 @@ if ($a.EngineAssociation -eq '4.27' -or $a.EngineAssociation -eq '4.26' -or $a.E
     $b | ConvertTo-Json > ../../../VRM4U/VRM4U.uplugin
 }
 
-if ($a.EngineAssociation -eq '5.0' -or $a.EngineAssociation -eq '4.27' -or $a.EngineAssociation -eq '4.26' -or $a.EngineAssociation -eq '4.25' -or $a.EngineAssociation -eq '4.24' -or $a.EngineAssociation -eq '4.23' -or $a.EngineAssociation -eq '4.22' -or $a.EngineAssociation -eq '4.21')
+if ($a.EngineAssociation -eq '5.0' -or $a.EngineAssociation -eq '4.27' -or $a.EngineAssociation -eq '4.26' -or $a.EngineAssociation -eq '4.25' -or $a.EngineAssociation -eq '4.24' -or $a.EngineAssociation -eq '4.23' -or $a.EngineAssociation -eq '4.22' -or $a.EngineAssociation -eq '4.21' -or $a.EngineAssociation -eq '4.20')
 {
 
     $b = Get-Content ../../../VRM4U/VRM4U.uplugin -Encoding UTF8 | ConvertFrom-Json
