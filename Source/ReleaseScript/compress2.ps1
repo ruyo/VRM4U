57477930827f843655be9836d14541a83f33b6ca
ruyo <ruyoruyo@gmail.com>
2024-03-23 00:02:46 +0900

    リリーススクリプト更新

--- Source/ReleaseScript/compress2.ps1
+++ Source/ReleaseScript/compress2.ps1
@@ -3,6 +3,8 @@ Remove-Item -Recurse ./_out/Intermediate
 Remove-Item -Recurse ./_out/Binaries/Win64/*.pdb
 Remove-Item -Recurse ./_out/Source/ReleaseScript
 
+Copy-Item -Path ../../ThirdParty -Destination ./_out -Recurse -Container
+
 Move-Item -Path ./_out -Destination ./Plugins
 
 Compress-Archive -Force -Path ./Plugins -DestinationPath $Args[0]
