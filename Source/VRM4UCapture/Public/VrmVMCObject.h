f6b2b79d0c3ba8996b0fe982476800a5d9654ddd
ruyo <ruyoruyo@gmail.com>
2024-05-28 23:29:55 +0900

    fix: ゲームビルドエラー

--- Source/VRM4UCapture/Public/VrmVMCObject.h
+++ Source/VRM4UCapture/Public/VrmVMCObject.h
@@ -3,11 +3,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "OSCServer.h"
 #include "Misc/EngineVersionComparison.h"
+#include "UObject/StrongObjectPtr.h"
+
 
 #include "VrmVMCObject.generated.h"
 
+class UOSCServer;
 //struct FOSCAddress;
 struct FOSCMessage;
 
