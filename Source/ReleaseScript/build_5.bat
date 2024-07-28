0776adafb7c31c2985a945b29a6fa08903facd8b
ruyo <ruyoruyo@gmail.com>
2024-07-28 20:17:17 +0900

    ビルドスクリプト 5.4エラー回避

--- Source/ReleaseScript/build_5.bat
+++ Source/ReleaseScript/build_5.bat
@@ -3,17 +3,34 @@ set V_DATE=%date:~0,4%%date:~5,2%%date:~8,2%
 set BUILD_SCRIPT=build_ver2.bat
 
 ::5_4
-call %BUILD_SCRIPT% 5.4 Win64 Shipping VRM4U_5_4_%V_DATE%.zip
+::call %BUILD_SCRIPT% 5.4 Win64 Shipping VRM4U_5_4_%V_DATE%.zip
+::if not %errorlevel% == 0 (
+::    echo [ERROR] :P
+::    goto err
+::)
+::call %BUILD_SCRIPT% 5.4 Android Development VRM4U_5_4_%V_DATE%_android.zip
+::if not %errorlevel% == 0 (
+::    echo [ERROR] :P
+::    goto err
+::)
+
+call build_ver.bat 5.4 Win64 Development MyProjectBuildScriptEditor VRM4U_5_4_%V_DATE%.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+call build_ver.bat 5.4 Win64 Development MyProjectBuildScript VRM4U_5_4_%V_DATE%_gam.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+call build_ver.bat 5.4 Win64 Shipping MyProjectBuildScript VRM4U_5_4_%V_DATE%_gam_shipping.zip
 if not %errorlevel% == 0 (
     echo [ERROR] :P
     goto err
 )
 
-call %BUILD_SCRIPT% 5.4 Android Development VRM4U_5_4_%V_DATE%_android.zip
-if not %errorlevel% == 0 (
-    echo [ERROR] :P
-    goto err
-)
+
 
 ::5_3
 call %BUILD_SCRIPT% 5.3 Win64 Shipping VRM4U_5_3_%V_DATE%.zip
