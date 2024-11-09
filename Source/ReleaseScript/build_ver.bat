7eb9783178999e18baceccce6db47b803fd1867c
ruyo <ruyoruyo@gmail.com>
2024-11-09 23:25:10 +0900

    リリーススクリプト更新

--- Source/ReleaseScript/build_ver.bat
+++ Source/ReleaseScript/build_ver.bat
@@ -51,10 +51,14 @@ if %UEMajorVersion% == 4 (
 )
 
 if %isUE4% == FALSE (
-del "..\..\..\VRM4U\Source\VRM4U\Public\VrmAssetUserData.h"
-del "..\..\..\VRM4U\Source\VRM4U\Private\VrmAssetUserData.cpp"
 del "..\..\..\VRM4U\Content\Util\Actor\latest\WBP_MorphTarget.uasset"
 )
+
+if %isUE4% == TRUE (
+del "..\..\..\VRM4U\Source\VRM4U\Public\VrmAssetUserData.h"
+del "..\..\..\VRM4U\Source\VRM4U\Private\VrmAssetUserData.cpp"
+)
+
 
 set isRetargeterEnable=TRUE
 if %UEMajorVersion% == 4 (
