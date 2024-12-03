c2643f23b28a5ba24b1008fc4115140611fd01c9
Jason Ma <1312119064@qq.com>
2024-12-03 20:50:58 +0800

    Fix UE 5.5 Installed Build errors

--- Source/VRM4UImporter/Private/VRM4UImporterFactory.cpp
+++ Source/VRM4UImporter/Private/VRM4UImporterFactory.cpp
@@ -281,7 +281,7 @@ UObject* UVRM4UImporterFactory::FactoryCreateBinary(UClass* InClass, UObject* In
 				ImportUI->bSingleUAssetFile = true;
 			}
 #else
-			// 5.2でも動くが、デフォルトをOFFにする
+			// 5.2縺ｧ繧ょ虚縺上′縲√ョ繝輔か繝ｫ繝医ｒOFF縺ｫ縺吶ｋ
 #endif
 
 
@@ -291,8 +291,8 @@ UObject* UVRM4UImporterFactory::FactoryCreateBinary(UClass* InClass, UObject* In
 			{
 				auto* s = GEditor->GetEditorSubsystem<UAssetEditorSubsystem>();
 				if (s && ImportUI->Thumbnail) {
-					s->OpenEditorForAsset(ImportUI->Thumbnail);
-					s->CloseAllEditorsForAsset(ImportUI->Thumbnail);
+					s->OpenEditorForAsset((UObject*)ImportUI->Thumbnail);
+					s->CloseAllEditorsForAsset((UObject*)ImportUI->Thumbnail);
 				}
 			}
 #endif
