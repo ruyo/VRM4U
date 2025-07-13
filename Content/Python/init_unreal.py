a2abdc161e5fcffdf0cf2eae186885026a613f27
ruyo <ruyoruyo@gmail.com>
2025-07-13 23:14:02 +0900

    game起動時にpythonのエラーが出るのを対処

--- Content/Python/init_unreal.py
+++ Content/Python/init_unreal.py
@@ -1,11 +1,29 @@
 import unreal
 
-editor_utility_subsystem = unreal.get_editor_subsystem(unreal.EditorUtilitySubsystem)
-editor_utility_object_path = "/VRM4U/Util/System/InitScript.InitScript"
+def isGame() -> bool:
 
-editor_utility_object = unreal.load_asset(editor_utility_object_path)
-if editor_utility_object:
-    editor_utility_subsystem.try_run(editor_utility_object)
-    print(f"VRMMenu Success: {editor_utility_object_path}")
-else:
-    print(f"VRMMenu Failed: {editor_utility_object_path}")
+    # pkg
+    if (unreal.SystemLibrary.is_packaged_for_distribution()):
+        return True
+
+    command_line = unreal.SystemLibrary.get_command_line()
+    if "-game" in command_line:
+        return True
+    elif "-server" in command_line:
+        return True
+
+    # editor
+    # is_editor = unreal.SystemLibrary.is_running_editor()
+
+    return False
+
+if (isGame() == False):
+    editor_utility_subsystem = unreal.get_editor_subsystem(unreal.EditorUtilitySubsystem)
+    editor_utility_object_path = "/VRM4U/Util/System/InitScript.InitScript"
+
+    editor_utility_object = unreal.load_asset(editor_utility_object_path)
+    if editor_utility_object:
+        editor_utility_subsystem.try_run(editor_utility_object)
+        print(f"VRMMenu Success: {editor_utility_object_path}")
+    else:
+        print(f"VRMMenu Failed: {editor_utility_object_path}")
