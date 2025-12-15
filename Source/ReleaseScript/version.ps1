d43f63404dc17e92b050e4627514cd8d3cbc2aba
ruyo <ruyoruyo@gmail.com>
2025-12-16 01:06:18 +0900

    buildscript

--- Source/ReleaseScript/version.ps1
+++ Source/ReleaseScript/version.ps1
@@ -29,7 +29,7 @@ if ($a.EngineAssociation -eq '4.27' -or $a.EngineAssociation -eq '4.26' -or $a.E
     $b | ConvertTo-Json > ../../../VRM4U/VRM4U.uplugin
 }
 
-if ($a.EngineAssociation -eq '5.0' -or $a.EngineAssociation -eq '4.27' -or $a.EngineAssociation -eq '4.26' -or $a.EngineAssociation -eq '4.25' -or $a.EngineAssociation -eq '4.24' -or $a.EngineAssociation -eq '4.23' -or $a.EngineAssociation -eq '4.22' -or $a.EngineAssociation -eq '4.21' -or $a.EngineAssociation -eq '4.20')
+if ($a.EngineAssociation -eq '5.1' -or $a.EngineAssociation -eq '5.0' -or $a.EngineAssociation -eq '4.27' -or $a.EngineAssociation -eq '4.26' -or $a.EngineAssociation -eq '4.25' -or $a.EngineAssociation -eq '4.24' -or $a.EngineAssociation -eq '4.23' -or $a.EngineAssociation -eq '4.22' -or $a.EngineAssociation -eq '4.21' -or $a.EngineAssociation -eq '4.20')
 {
 
     $b = Get-Content ../../../VRM4U/VRM4U.uplugin -Encoding UTF8 | ConvertFrom-Json
