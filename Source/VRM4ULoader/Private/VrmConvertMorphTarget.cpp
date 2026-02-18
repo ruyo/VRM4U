0b8c46e4865ac9d872f3fec39462e796c41e7674
ruyo <ruyoruyo@gmail.com>
2026-02-18 22:28:57 +0900

    ビルドエラー修正

--- Source/VRM4ULoader/Private/VrmConvertMorphTarget.cpp
+++ Source/VRM4ULoader/Private/VrmConvertMorphTarget.cpp
@@ -352,7 +352,7 @@ bool VRMConverter::ConvertMorphTarget(UVrmAssetListObject *vrmAssetList) {
 					MorphLODModel.Vertices = MorphDeltas;
 
 #if	UE_VERSION_OLDER_THAN(5,0,0)
-					if (mt->MorphLODModels().IsValidIndex(0) == false) {
+					if (mt->MorphLODModels.IsValidIndex(0) == false) {
 						mt->MorphLODModels.Add(MorphLODModel);
 					} else {
 						mt->MorphLODModels[0] = MorphLODModel;
